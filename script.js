5077383b67b1dc75d08d358b496177e7d1d80906
dainikdokan <dainikdokan@gmail.com>
2024-11-11 21:52:17 +0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showSection(sectionId) {
-    const sections = document.querySelectorAll('.section');
-    sections.forEach(section => section.classList.remove('active'));
-    document.getElementById(sectionId).classList.add('active');
-
-    const navLinks = document.querySelectorAll('.navbar a');
-    navLinks.forEach(link => link.classList.remove('active'));
-    document.querySelector(`[onclick="showSection('${sectionId}')"]`).classList.add('active');
-}
